fc1824fa76a6a65e51acbda1b24004ec72c8f1f9
EtinRuyi <omoruyi.idowu@yahoo.com>
2024-01-23 00:04:21 +0100

    BaseEntity

---
+++ HeightsAuction.Domain/Entities/SharedEntities/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HeightsAuction.Domain.Entities.SharedEntities
+{
+    public class BaseEntity
+    {
+        [Required]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
