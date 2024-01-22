ffbaedc7bf1cb4650737de70c616937e1e80a64c
EtinRuyi <omoruyi.idowu@yahoo.com>
2024-01-22 13:51:51 +0100

    Folder-Setup

--- HeightsAuction.API/obj/Debug/net7.0/HeightsAuction.API.AssemblyInfo.cs
+++ HeightsAuction.API/obj/Debug/net7.0/HeightsAuction.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeightsAuction.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3a8f3849ee2e0e97c83048fce1fa23afc2d92c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db23763da242145b7f6878c88d341db82c69eba2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeightsAuction.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeightsAuction.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
