bc1b9589fbd537a79baf047098de914e4fa07c35
EtinRuyi <omoruyi.idowu@yahoo.com>
2024-01-23 00:16:11 +0100

    AppUser

---
+++ HeightsAuction.Domain/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HeightsAuction.Domain.Entities
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsDeleted { get; set; }
+    }
+}
