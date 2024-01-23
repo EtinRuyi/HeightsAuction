d1339ba4157f38c74f08bdad7beb5235af852484
EtinRuyi <omoruyi.idowu@yahoo.com>
2024-01-23 02:26:24 +0100

    HAuctionDbContext

--- HeightsAuction.API/obj/Debug/net7.0/HeightsAuction.API.AssemblyInfo.cs
+++ HeightsAuction.API/obj/Debug/net7.0/HeightsAuction.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeightsAuction.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6908e47fa22bb945bd1ddbbe54c693bab39f482a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0141d78ecd8e39f96b49a3ee79b321cabd50d4c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeightsAuction.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeightsAuction.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ HeightsAuction.Persistence/AppContext/HAuctionDBContext.cs
@@ -0,0 +1,17 @@
+﻿using HeightsAuction.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeightsAuction.Persistence.AppContext
+{
+    public class HAuctionDBContext : IdentityDbContext<AppUser>
+    {
+        public HAuctionDBContext(DbContextOptions<HAuctionDBContext> options) : base(options) { }
+
+        public DbSet<Bid> Bids { get; set; }
+        public DbSet<BiddingRoom> BiddingRooms { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<BidNotification> BidNotifications { get; set; }
+    }
+}
