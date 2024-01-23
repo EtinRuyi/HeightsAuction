b6cbfcd7d58637dc7521091a87434107ed7925ba
EtinRuyi <omoruyi.idowu@yahoo.com>
2024-01-23 02:55:52 +0100

    IGenericRepository

---
+++ HeightsAuction.Application/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace HeightsAuction.Application.Interfaces.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(string id);
+        Task<List<T>> GetAllAsync();
+        Task<List<T>> FindAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void DeleteAsync(T entity);
+        void DeleteAllAsync(List<T> entities);
+        void SaveChangesAsync();
+        Task<bool> ExistsAsync(Expression<Func<T, bool>> expression);
+    }
+}
