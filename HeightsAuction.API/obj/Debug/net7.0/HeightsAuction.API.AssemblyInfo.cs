a6e8c02b14198ec5cf988d000e0e31442164024f
EtinRuyi <omoruyi.idowu@yahoo.com>
2024-02-02 14:52:14 +0100

    Quick-Fix

--- HeightsAuction.API/obj/Debug/net7.0/HeightsAuction.API.AssemblyInfo.cs
+++ HeightsAuction.API/obj/Debug/net7.0/HeightsAuction.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeightsAuction.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0e26627720e6cfdc20398d2c33b464671799039")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcd6dce7ea86ef4444ae48278989a0ad0a8f737d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeightsAuction.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeightsAuction.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
