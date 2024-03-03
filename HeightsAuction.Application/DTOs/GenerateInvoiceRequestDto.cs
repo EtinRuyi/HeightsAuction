22d19d261d9771c33aff4ad2133a96454a76e502
EtinRuyi <omoruyi.idowu@yahoo.com>
2024-03-03 03:40:02 +0100

    Quick-Fix

--- HeightsAuction.Application/DTOs/GenerateInvoiceRequestDto.cs
+++
@@ -1,8 +0,0 @@
-﻿//namespace HeightsAuction.Application.DTOs
-//{
-//    public class GenerateInvoiceRequestDto
-//    {
-//        public string BiddingRoomId { get; set; }
-//        public string UserId { get; set; }
-//    }
-//}
